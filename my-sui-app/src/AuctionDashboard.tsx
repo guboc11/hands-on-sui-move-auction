911b63b490b7415b21b05447caaebebcb5a4f4f9
guboc11 <guboc11@gmail.com>
2025-09-14 05:37:16 +0900

    add create_item tx

--- my-sui-app/src/AuctionDashboard.tsx
+++ my-sui-app/src/AuctionDashboard.tsx
@@ -14,6 +14,10 @@ import {
 import { OwnedObjects } from "./OwnedObjects";
 import { AuctionObjects } from "./AuctionObjects";
 import { useState } from "react";
+import { Transaction } from "@mysten/sui/transactions";
+
+const PACKAGE =
+  "0xbf3dd44668fcc8cbc740f8a3ed5fb2af0d02abbe3a8fbf452331921e82dfc6b9";
 
 export function AuctionDashboard() {
   const account = useCurrentAccount();
@@ -35,7 +39,19 @@ export function AuctionDashboard() {
           </Flex>
           <Flex direction="column" gap="4">
             <Box>
-              <Button>create_item</Button>
+              <Button
+                onClick={() => {
+                  const tx = new Transaction();
+                  tx.moveCall({
+                    package: PACKAGE,
+                    module: "auction",
+                    function: "create_item",
+                  });
+                  signAndExcuteTransaction({ transaction: tx });
+                }}
+              >
+                create_item
+              </Button>
             </Box>
             <Flex gap="4">
               <Button>create_auction</Button>
